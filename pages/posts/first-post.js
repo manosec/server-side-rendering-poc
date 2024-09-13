f406ba5204536db6bdd63dc840ffd70438913747
manosec <manoranjanbca2019@gmail.com>
2024-09-13 10:12:13 +0530

    Added Image Script and Css module functionalities in nextjs

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,34 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/Layout";
 
 export default function FirstPost(){
     return (
-        <div>
-        <h1>First Post</h1>
+        <Layout>            
+            <Head>
+                <title>First Next Page</title>
+            </Head>
+            <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+                console.log(`script loaded correctly, window.FB has been populated`)
+            }
+            >
+
+            </Script>
+            <Image
+                src="/images/profile.jpg"
+                height={144}
+                width={144}
+                alt="seo keywords"
+                >
+
+            </Image>
+            <h1>First Post</h1>
             <Link href="/"> <h2>Back Home</h2> </Link>
-        </div>
+        </Layout>
     )
 }
